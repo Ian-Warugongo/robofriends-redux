f22713173a308118958a4b729deafa0b057b136f
Ian-Warugongo <ian.k.warugongo@gmail.com>
2025-05-29 20:30:38 +0300

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import './index.css';
 import App from './containers/App.jsx';
+import { searchRobots } from './reducers.jsx';
 import 'tachyons';
+
+const store = createStore(searchRobots)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
